5b190d309bdb64f60e1b44eaf4eb0eedadf79b26
Ole Borup <obo@frv.dk>
2012-01-10 13:35:30 +0100

    More demo setuP

--- demo/route_inject.bat
+++
@@ -1,5 +0,0 @@
-
-cd ..\bin\
-route_inject.bat ..\demo\ais_dump_20110113_17_00.txt ..\demo\ais_dump_intenteded_route.txt 219005932,259655000,304786000,248139000,506026000,240244000,518120000,255801540
-cd ..\demo\
-
